b1924384817c0966bf008b1b27e69da9507b80ed
Darkhaa <infinityhazard@gmail.com>
2024-02-21 02:51:36 +0000

    again

--- pages/index.js
+++ pages/index.js
@@ -73,7 +73,7 @@ const Home = () => (
           Work
         </Heading>
         <Paragraph>
-          Darkhanbayar is is full-stack developer based at Ulaanbaatar with a
+          Darkhanbayar is is is full-stack developer based at Ulaanbaatar with a
           passion for building digital services/stuff he wants. He has
           interested in all things launching products, from planning and
           designing all the way to solving real-life problems with code.
